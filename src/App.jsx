d60925ace17e79cefb4d0d7f68414099c375e42d
Faye Bishop <fayebishop44@gmail.com>
2025-04-07 18:33:17 -0500

    fix hosting issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay'
-import MoviesDisplay from './components/MoviesDisplay/MoviesDisplay.jsx/MoviesDisplay'
+// import MoviesDisplay from './components/MoviesDisplay/MoviesDisplay.jsx/MoviesDisplay'
 
 const App = () => {
   return (
