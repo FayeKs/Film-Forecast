77adba2265348b3eafcd9f9812faedf1e0818457
Faye Bishop <fayebishop44@gmail.com>
2025-04-07 19:12:13 -0500

    fix hosting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "Film-Forecast",
   server: {
     port: 5500,
     proxy: {
